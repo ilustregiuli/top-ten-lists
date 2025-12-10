16a64537c30aae8cef952103f77e392463249e51
Giuli Ilustre <ilustre.giuli@gmail.com>
2025-12-09 21:46:12 -0300

    Colocado sombra nos inputs de texto

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -8,10 +8,10 @@
             <form action="{{ route('entrar') }}" method="post" autocomplete="off">
                 @csrf
                 <label for="username">Usuário:</label><br>
-                <input type="text" id="username" name="username" required autocomplete="off"><br><br>
+                <input type="text" id="username" name="username" required autocomplete="off" class="w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring-indigo-500 bg-white"><br><br>
 
                 <label for="password">Senha:</label><br>
-                <input type="password" id="password" name="password" required autocomplete="off"><br><br>
+                <input type="password" id="password" name="password" required autocomplete="off" class="w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring-indigo-500 bg-white"><br><br>
 
                 <button type="submit">Entrar</button>
             </form>
